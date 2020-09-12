9aca7110c82dcb8e1d713682ea528ef38d225995
nguyencongluat <nguyencongluat092001@gmail.com>
2020-09-12 22:43:31 +0700

    lap2_06.cpp

--- Desktop/C/lap2_03.cpp
+++ Desktop/C/lap2_03.cpp
@@ -1,23 +1,17 @@
 #include <stdio.h>
 int main(){
-	int num;
-	printf("Nhap So Num = \n");
-	scanf("%d",&num);
-	printf("Binh Phuong Cua SO Do:%d ",num * num);
-	int num1;
-	int num2;
-	printf("Nhap So Num1 = \n");
-	scanf("%d",&num1);
-printf("Nhap So Num2= \n");
-	scanf("%d",&num2);
-		int tong = 0;
-	int hieu = 0;
-	int tich = 0;
-	tong = num1 + num2;
-	hieu = num1 - num2;
-	tich = num1 * num2;   return 0;
-	printf("Tong:%d \n",tong);
-		printf("tich:%d \n",tich);
-	printf("hieu:%d \n",hieu);
+	float DiemToan;
+	printf("Nhap DiemToan = \n");
+	scanf("%f",&DiemToan);
+	float DiemLy;
+	printf("Nhap DiemLy = \n");
+	scanf("%f",&DiemLy);
+	float DiemHoa;
+	printf("Nhap DiemHoa = \n");
+	scanf("%f",&DiemHoa);
+	// float sum= 0;
+	// sum =(DiemToan + DiemHoa + DiemLy)/3;
+	printf("Diem Trung Binh: %f \n", (DiemToan + DiemHoa + DiemLy)/3 );
+	
 }
 
